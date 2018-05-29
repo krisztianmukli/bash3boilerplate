0342bcbc15936e4aa50e48eb895e3088b9ec7303
Mukli Krisztián <krisztianmukli@mukli.hu>
2018-05-29 22:03:37 +0200

    Controlled installation of libs Warning message if reloading ~/.profile need

--- lib/install_script.sh
+++ lib/install_script.sh
@@ -11,7 +11,7 @@ local unsupported_message=$"Install script manually:
   source display_info.sh
   [[ "${DISTROTYPE+x}" ]] || source os_detection.sh
 
-  libraries=$( ini_val "${sourcedir}/b3bp_install.ini" install.libs )
+  libs=$( ini_val "${sourcedir}/b3bp_install.ini" install.libs )
   if [[ "${sourcedir:-}/${scriptfile:-}" ]]; then
     # Check OS version
     if [[ "${OSTYPE}" = "linux-gnu" && "${DISTROTYPE:-}" = "debian-based" ]]; then
@@ -19,35 +19,46 @@ local unsupported_message=$"Install script manually:
         targetdir="${HOME}/.local/bin"
         localedir="${HOME}/.local/share/locale"        
 
-	# Check and create destination
+	      # Check and create destination
         [[ ! -d "${targetdir}" ]] && mkdir "${targetdir}"
-	[[ ! -d "${targetdir}/lib" && -d "${sourcedir}/lib" ]] && mkdir "${targetdir}/lib"
+        [[ ! -d "${targetdir}/lib" && -d "${sourcedir}/lib" ]] && mkdir "${targetdir}/lib"
         [[ ! -d "${localedir}" && -d "${sourcedir}/locale" ]] && mkdir "${localedir}"
 
         # Copy script to ~/.local/bin
         cp -i "${sourcedir}/${scriptfile}" "${targetdir}"
-	for lib in "${libraries}"; do
-	  cp -i "${sourcedir}/lib/${lib}" "${targetdir}/lib"
-	done 	
+	      for lib in $libs; do
+	        cp -i "${sourcedir}/lib/${lib}" "${targetdir}/lib"
+	      done 	
         #cp -ir "${sourcedir}/lib/." "${targetdir}/lib"
         cp -ir "${sourcedir}/locale/." "${localedir}"
 
-        # Debian-based systems already contains user private bin in ~/.profile
+        # Ubuntu-based systems already contains user private bin in ~/.profile, but Debian not
+        if ! grep -q "\$HOME/.local/bin" "${HOME}/.profile"; then
+          {
+            echo "" 
+            echo "# set PATH so it includes user's private bin if it exists"
+            echo "if [ -d \"\$HOME/.local/bin\" ] ; then"
+            echo "    PATH=\"\$HOME/.local/bin:\$PATH\" "
+            echo "fi" 
+          } >> "${HOME}/.profile"
+          warning "Run 'source ~/.profile' command to reload PATH variable!"
+        fi
+        
         source "${HOME}/.profile" && export PATH
-	info "Installation was succesful to ${targetdir}"
+        info "Installation was succesful to ${targetdir}"
       else
         targetdir="/usr/local/bin"
         localedir="/usr/share/locale"
 
         # Copy script to ~/.local/bin
         cp -i "${sourcedir}/${scriptfile}" "${targetdir}"
-	for lib in "${libraries}"; do
-	  cp -i "${sourcedir}/lib/${lib}" "${targetdir}/lib"
-	done 	
+        for lib in $libs; do
+          cp -i "${sourcedir}/lib/${lib}" "${targetdir}/lib"
+        done 	
         #cp -ir "${sourcedir}/lib/." "${targetdir}/lib"
         cp -ir "${sourcedir}/locale/." "${localedir}"
 
-	info "Installation was succesful to ${targetdir}"
+        info "Installation was succesful to ${targetdir}"
       fi
 
     else
@@ -55,6 +66,6 @@ local unsupported_message=$"Install script manually:
     fi
 
   fi
-
+  
   exit 1
 }
