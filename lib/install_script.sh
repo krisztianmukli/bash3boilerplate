8a5b207eea4fe51aad0d56c4b4abdafe6f7915b3
Mukli Krisztián <krisztianmukli@mukli.hu>
2018-05-30 12:15:21 +0200

    More sofisticated way to set TEXTDOMANDIR b3bp_install.ini renamed to install.ini copy_files function add LOG_LEVEL and NO_COLOR to log.sh

--- lib/install_script.sh
+++ lib/install_script.sh
@@ -1,36 +1,40 @@
 #!/usr/bin/env bash
-function install_script() { # __b3bp_dir __b3bp_base
+
+function copy_files() {
+local sourcedir="${1:-}"
+local targetdir="${2:-}"
+
+  libs=$( ini_val "${sourcedir}/b3bp_install.ini" install.libs )
+  # Check and create destination
+  [[ ! -d "${targetdir}" ]] && mkdir "${targetdir}"
+  [[ ! -d "${targetdir}/lib" && -d "${sourcedir}/lib" ]] && mkdir "${targetdir}/lib"
+  [[ ! -d "${localedir}" && -d "${sourcedir}/locale" ]] && mkdir "${localedir}"
+
+  # Copy files from sourcedir to targetdir
+  cp -i "${sourcedir}/${scriptfile}" "${targetdir}"
+        for lib in $libs; do
+          cp -i "${sourcedir}/lib/${lib}" "${targetdir}/lib"
+        done 	
+  cp -ir "${sourcedir}/locale/." "${localedir}"
+
+}
+
+function install_script() {
 local sourcedir="${1:-}"
 local scriptfile="${2:-}"
-local unsupported_header=$"Unsupported operating system."
-local unsupported_message=$"Install script manually: 
- * Copy the script and the lib folder to your PATH.
- * Copy to locale folder to your locale folder."
 
   source ini_val.sh
   source display_info.sh
   [[ "${DISTROTYPE+x}" ]] || source os_detection.sh
 
-  libs=$( ini_val "${sourcedir}/b3bp_install.ini" install.libs )
   if [[ "${sourcedir:-}/${scriptfile:-}" ]]; then
     # Check OS version
     if [[ "${OSTYPE}" = "linux-gnu" && "${DISTROTYPE:-}" = "debian-based" ]]; then
       if [[ $EUID != 0 ]]; then
         targetdir="${HOME}/.local/bin"
         localedir="${HOME}/.local/share/locale"        
-
-	      # Check and create destination
-        [[ ! -d "${targetdir}" ]] && mkdir "${targetdir}"
-        [[ ! -d "${targetdir}/lib" && -d "${sourcedir}/lib" ]] && mkdir "${targetdir}/lib"
-        [[ ! -d "${localedir}" && -d "${sourcedir}/locale" ]] && mkdir "${localedir}"
-
-        # Copy script to ~/.local/bin
-        cp -i "${sourcedir}/${scriptfile}" "${targetdir}"
-	      for lib in $libs; do
-	        cp -i "${sourcedir}/lib/${lib}" "${targetdir}/lib"
-	      done 	
-        #cp -ir "${sourcedir}/lib/." "${targetdir}/lib"
-        cp -ir "${sourcedir}/locale/." "${localedir}"
+	
+        copy_files "${sourcedir}" "${targetdir}"
 
         # Ubuntu-based systems already contains user private bin in ~/.profile, but Debian not
         if ! grep -q "\$HOME/.local/bin" "${HOME}/.profile"; then
@@ -41,28 +45,28 @@ local unsupported_message=$"Install script manually:
             echo "    PATH=\"\$HOME/.local/bin:\$PATH\" "
             echo "fi" 
           } >> "${HOME}/.profile"
-          warning "Run 'source ~/.profile' command to reload PATH variable!"
+          warning "~/.profile was updated, please login again or run 'source ~/.profile' command to reload PATH variable!"
         fi
         
         source "${HOME}/.profile" && export PATH
         info "Installation was succesful to ${targetdir}"
       else
         targetdir="/usr/local/bin"
-        localedir="/usr/share/locale"
+        localedir="/usr/local/share/locale"
 
-        # Copy script to ~/.local/bin
-        cp -i "${sourcedir}/${scriptfile}" "${targetdir}"
-        for lib in $libs; do
-          cp -i "${sourcedir}/lib/${lib}" "${targetdir}/lib"
-        done 	
-        #cp -ir "${sourcedir}/lib/." "${targetdir}/lib"
-        cp -ir "${sourcedir}/locale/." "${localedir}"
-
+        copy_files "${sourcedir}" "${targetdir}"
         info "Installation was succesful to ${targetdir}"
       fi
 
     else
-      display_info "${unsupported_header}" "${unsupported_message}"
+      local unsupported_header=$"Unsupported operating system."
+      local unsupported_message=$"Install script manually: 
+ * Copy the script and the lib folder to your PATH.
+ * Copy to locale folder to your locale folder."
+      
+      display_info $"Unsupported operating system." $"Install script manually: 
+ * Copy the script and the lib folder to your PATH.
+ * Copy to locale folder to your locale folder."
     fi
 
   fi
